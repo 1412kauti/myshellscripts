9643b4bc7eafc84b59fe41024b92b5d004c6549c
Kautilya Reddy <kautilya97@gmail.com>
2023-10-09 22:48:45 -0400

    Update download_mambaforge.sh
    Switched to Miniforge due to deprecation of Mambaforge

--- download_mambaforge.sh
+++ download_mambaforge.sh
@@ -1,3 +1,3 @@
-axel --output=Mambaforge3.sh https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-x86_64.sh
-bash Mambaforge3.sh -b -p "${HOME}/conda"
-rm Mambaforge3.sh
+axel --output=Miniforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
+bash Miniforge3.sh -b -p "${HOME}/conda"
+rm Miniforge3.sh
