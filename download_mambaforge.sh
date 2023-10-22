4f6bd11ca84ecb964863b2ce759702fb709fbbd8
Kautilya Reddy Chappidi <kautilya97@gmail.com>
2023-10-22 05:28:59 -0400

    fix mamba

--- download_mambaforge.sh
+++ download_mambaforge.sh
@@ -1,3 +1,3 @@
-wget -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh
+axel --output=Mambaforge3.sh "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
 bash Miniforge3.sh -b -p "${HOME}/conda"
 rm Miniforge3.sh
