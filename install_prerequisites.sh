2ff7585f2175aebde9a6ca8ffc8c9b3ff94b0697
Kautilya Reddy <kautilya97@gmail.com>
2026-09-09 11:59:24 +0530

    Update install_prerequisites.sh to remove packages
    Removed 'btrfs-progs' and 'virt-manager' from the installation list.

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,1 +1,1 @@
-sudo apt install -y git curl wget libfuse2t64 gnome-tweaks gnome-sushi gnome-shell-extension-manager openssh-server zip cargo btrfs-progs cmake npm virt-manager qemu-system
+sudo apt install -y git curl wget libfuse2t64 gnome-tweaks gnome-sushi gnome-shell-extension-manager openssh-server zip cargo cmake npm
